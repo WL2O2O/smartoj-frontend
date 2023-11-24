2b6ae0082e6974b18e1598e145f10bd103171c1c
WLei224 <1149630057@qq.com>
2023-11-24 10:31:34 +0800

    Feat: DoDevelop vue-frontend-init Template

---
+++ src/views/NoAuthView.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="home">
+    你没权限，为您献上一颗安全坤蛋-->OJ
+    <img alt="Vue logo" style="height: 20px" src="../assets/oj.png" />
+  </div>
+</template>
+
+<script lang="ts"></script>
