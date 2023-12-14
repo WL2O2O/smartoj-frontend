53d3aa5b02146f42477f5085aff2d3d4abfee8a3
WLei224 <1149630057@qq.com>
2023-12-14 13:14:08 +0800

    amend access bug

--- src/access/index.ts
+++ src/access/index.ts
@@ -5,11 +5,16 @@ import checkAcess from "@/access/checkAcess";
 
 router.beforeEach(async (to, from, next) => {
   console.log("登陆用户信息", store.state.user.loginUser);
-  const loginUser = store.state.user.loginUser;
+  let loginUser = store.state.user.loginUser;
   // 如果之前没登陆过，自动登录
-  if (!loginUser || !loginUser.userRole) {
+  if (
+    !loginUser ||
+    !loginUser.userRole ||
+    loginUser.userRole === ACCESS_ENUM.NOT_LOGIN
+  ) {
     // 加 await 是为了等用户登录成功之后，再执行后续的代码
     await store.dispatch("user/getLoginUser");
+    loginUser = store.state.user.loginUser;
   }
   const needAccess = (to.meta?.access as string) ?? ACCESS_ENUM.NOT_LOGIN;
   // 要跳转的页面必须要登陆
